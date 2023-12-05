f9bebe9c69da31c2a70983e6ca3863c2462ce7b1
oliceglad <vycheslavofficial@gmail.com>
2023-12-05 16:21:32 +0400

    added data

---
+++ my-app/src/data.js
@@ -0,0 +1,14 @@
+export const data = [
+    {
+        id: 0,
+        name: 'Имя товара',
+        rating: '3.5',
+        price: 19000,
+    },
+    {
+        id: 1,
+        name: 'Имя товара',
+        rating: '3.5',
+        price: 19000,
+    }
+]
