56ec75b74655164358e4ad9c921a7fbbda2740a0
SoNya Marker <soniamarker13@gmail.com>
2023-12-05 15:38:24 +0300

    footer

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import { Header } from './Components/Header';
 import Banners from './Components/Banners';
+import Footer from './Components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Banners />
+
+      <Footer />
     </div>
   );
 }
