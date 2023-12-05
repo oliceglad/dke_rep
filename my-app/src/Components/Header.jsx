60f2b012c5750a977aa9343228b51912b9aa90da
oliceglad <vycheslavofficial@gmail.com>
2023-12-05 16:18:43 +0400

    added icons

--- my-app/src/Components/Header.jsx
+++ my-app/src/Components/Header.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import { Box, Button, InputGroup, InputRightElement } from '@chakra-ui/react'
-import { SearchIcon } from '@chakra-ui/icons'
+import { SearchIcon, ChatIcon } from '@chakra-ui/icons'
 import { Input } from '@chakra-ui/react'
+import { CiUser } from "react-icons/ci"
+import { IoBagHandleOutline } from "react-icons/io5";
+import { FaCodeCompare } from "react-icons/fa6"
+import { CiHeart } from "react-icons/ci";
 
 
 export const Header = () => {
@@ -15,32 +19,44 @@ export const Header = () => {
                     +7 939 700 87 32
                 </Box>
             </Box>
-            <Box bg='#0050b3' display='flex' justifyContent='space-between' padding={10}>
+            <Box bg='#0050b3' display='flex' justifyContent='space-between' alignItems='center' padding={10}>
                 <Box>
                     Logo
                 </Box>
-                <Box>
+                <Box display='flex'>
                     <Button>
                         Каталог
                     </Button>
-                    <InputGroup display='flex'>
-                        <Input width='auto' borderRadius={10} padding={5} marginLeft={10} />
+                    <InputGroup>
+                        <Input width='auto' borderRadius={10} padding={5} />
                         <InputRightElement>
-                            <SearchIcon color='black' marginTop={5}/>
+                            <SearchIcon color='black' marginTop={5} />
                         </InputRightElement>
                     </InputGroup>
                 </Box>
                 <Box>
-                    Избранное
+                    <CiHeart />
+                    <Box>
+                        Избранное
+                    </Box>
                 </Box>
                 <Box>
-                    Сравнение
+                    <FaCodeCompare />
+                    <Box>
+                        Сравнение
+                    </Box>
                 </Box>
                 <Box>
-                    Корзина
+                    <IoBagHandleOutline />
+                    <Box>
+                        Корзина
+                    </Box>
                 </Box>
                 <Box>
-                    Войти
+                    <CiUser />
+                    <Box>
+                        Войти
+                    </Box>
                 </Box>
             </Box>
         </Box>
